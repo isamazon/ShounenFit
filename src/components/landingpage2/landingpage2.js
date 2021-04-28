4481817351ed835aa9f60520289d233001e4aaf7
isamazon <37008425+isamazon@users.noreply.github.com>
2021-04-28 16:00:01 -0600

    Completely redesigning the second home page
    in the process of replacing the original home page

--- src/components/landingpage2/landingpage2.js
+++ src/components/landingpage2/landingpage2.js
@@ -18,30 +18,9 @@ function Landingpage2() {
 
     return (
         <Container fluid className='page-2-fluid'>
-            <Container className='page-2-container'>
-                <Row className='row-1'>
-                    <Col xl={6} sm={12} className='col-1'>
-                        <Heroslide />
-                        <h1 data-aos='fade-down'
-                        
-                        data-aos-duration='1500'
-                        >How does it work?</h1>
-                        <p data-aos="fade-down"
-                        
-                        data-aos-duration='1500'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis recusandae tempora laboriosam officiis aliquam dolores debitis fugit est totam natus odit quo saepe, facilis minima at dolor, dolorum, veritatis illo!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sint in neque maiores architecto veniam, totam optio dolor dolore rerum, perspiciatis commodi, hic sequi quam recusandae enim fugit. Sit, inventore.
-                        </p>
-                    </Col>
-                    <Col xl={6} sm={12} className='col-2'>
-                        <h1 data-aos='fade-up'
-                        data-aos-delay='500'
-                        data-aos-duration='1000'>Why Shounen fit?</h1>
-                        <p data-aos='fade-down'
-                        data-aos-delay='800'
-                        data-aos-duration='1500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis placeat ex quo eligendi eius ullam. Quo aliquid quaerat, temporibus quia saepe deleniti distinctio obcaecati ullam perspiciatis, suscipit est incidunt unde.</p>
-                    </Col>
-                </Row>
-            </Container>
+            <Row ClassName='row-1'>
+
+            </Row>
         </Container>
     )
 }
