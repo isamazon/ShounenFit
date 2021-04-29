6194b4963ede949118fa8218d05e6473b42e378f
isamazon <37008425+isamazon@users.noreply.github.com>
2021-04-28 21:24:27 -0600

    First version of landingpage 2 done
    Added the styling and background video for the second page

--- src/components/landingpage2/landingpage2.js
+++ src/components/landingpage2/landingpage2.js
@@ -1,28 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 // CSS
 import  '../../CSS/landingpage2/landingpage2.css'
-import '../../../node_modules/aos/dist/aos.css'
 // Dependancies
 import { Container, Col, Row} from 'react-bootstrap'
 // Components
-import AOS from 'aos'
 import Heroslide from '../landingpage2/heroslide'
+// Character imgs
+import deku from '../../assets/dekupng.png'
+import video1 from '../../assets/circlevid.mp4'
 
 
-function Landingpage2() {
+class Landingpage2 extends Component {
+    render() {
+        return (
+            <Container fluid className='page-2-fluid'>
+                <Row className='row-1'>
+                    <Col md={7} xs={12} className='col-2'>
+                    <div className='char-slider'>
+                            {/* video goes here */}
+                            <video className='video1'
+                            src={video1}
+                            type="video/mp4"
+                            autoPlay
+                            loop></video>
+                            <img className='char-img' src={deku} alt=""/>
+                        </div>
+                        
+                    </Col>
+                    <Col md={5} xs={12} className='col-1'>
+                    <h1>Why shounen fit?</h1>
+                    <p>Almost every anime fan cant help to be inspired by every shounen protagonist that faces obstacles and overcomes them. Using anime as the cornerstone of this project is to inspire all anime fans to reach their goals and become the best shoune the can become</p>
+                    </Col>
+                </Row>
+            </Container>
 
-    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
-      }, []);
-
-    return (
-        <Container fluid className='page-2-fluid'>
-            <Row ClassName='row-1'>
-
-            </Row>
-        </Container>
-    )
+        )
+    }
 }
 
 export default Landingpage2
