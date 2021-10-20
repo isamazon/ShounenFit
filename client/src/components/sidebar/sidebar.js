89aa4682ac53b6b82feeda20ee45290b4635e18f
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-10-20 14:33:23 -0600

    Sidebar menu built
    Added links for sidebar menu to allow user to navigate the site with ease
    Home to go to home
    Dashboard to go to dashboard
    Workouts opens up and displays links to:
    adding a new workout,
    recent workouts,
    Favorite workouts,

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sidebar.css';
 // Dependencies
 import { Link } from 'react-router-dom';
 // icons
 import { GrAddCircle } from 'react-icons/gr';
-import { FaDumbbell, FaHome, FaQuestion, FaRegHeart } from 'react-icons/fa';
+import {
+  FaDumbbell,
+  FaHome,
+  FaQuestion,
+  FaRegHeart,
+  FaHistory,
+  FaCog,
+  FaCaretRight,
+  FaPlus,
+} from 'react-icons/fa';
+import { FiLogOut } from 'react-icons/fi';
+import { BsGridFill } from 'react-icons/bs';
 function Sidebar(props) {
+  const [sideMenu, setSideMenu] = useState(false);
+  const ToggleMenu = () => {
+    setSideMenu(!sideMenu);
+  };
   return (
     <div className="sidebar-container">
       <div className="inner-sidebar">
         <h1 className="shonenfit-icon">SF</h1>
         <div className="side-bar-items">
-          <Link className="side-bar-item">
+          <Link to="/" className="side-bar-item">
             <FaHome className="sidebar-icon" style={props.iconcolor} />
             <p className="sidebar-p">Home</p>
           </Link>
-          <Link className="side-bar-item">
+          <Link to="/" className="side-bar-item">
+            <BsGridFill className="sidebar-icon" style={props.iconcolor} />
+            <p className="sidebar-p">Dashboard</p>
+          </Link>
+          <div className="side-bar-item" onMouseEnter={ToggleMenu}>
             <FaDumbbell className="sidebar-icon" style={props.iconcolor} />
             <p className="sidebar-p">Workouts</p>
-          </Link>
+            <FaCaretRight className="openmenu-icon" style={props.iconcolor} />
+          </div>
+          {/* Side Menu */}
+          <div
+            className={sideMenu ? 'side-menu' : 'none'}
+            onMouseLeave={ToggleMenu}
+          >
+            <Link className="side-bar-item first">
+              <FaPlus
+                className="sidebar-icon question"
+                style={props.iconcolor}
+              />
+              <p className="sidebar-p">New workout</p>
+            </Link>
+            <Link className="side-bar-item">
+              <FaHistory
+                className="sidebar-icon question"
+                style={props.iconcolor}
+              />
+              <p className="sidebar-p">Recent</p>
+            </Link>
+            <Link className="side-bar-item last">
+              <FaRegHeart
+                className="sidebar-icon question"
+                style={props.iconcolor}
+              />
+              <p className="sidebar-p">Favorites</p>
+            </Link>
+          </div>
           <Link className="side-bar-item">
             <FaQuestion
               className="sidebar-icon question"
@@ -26,12 +73,18 @@ function Sidebar(props) {
             />
             <p className="sidebar-p">How to use</p>
           </Link>
-          <Link className="side-bar-item">
-            <FaRegHeart
+        </div>
+        <div className="lowerlinks">
+          <Link className="bottom-items">
+            <FaCog className="sidebar-icon question" style={props.iconcolor} />
+            <p className="sidebar-p">Settings</p>
+          </Link>
+          <Link className="bottom-items">
+            <FiLogOut
               className="sidebar-icon question"
               style={props.iconcolor}
             />
-            <p className="sidebar-p">Favorites</p>
+            <p className="sidebar-p">Logout</p>
           </Link>
         </div>
       </div>
